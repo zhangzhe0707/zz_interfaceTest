1a8ac2d6b2c44a83b0e43e85fc91f2b554c4a095
zhangzhe <zhangzhe@haima.me>
2019-04-01 21:31:14 +0800

    test

--- base/baseCode.py
+++ base/baseCode.py
@@ -1,14 +1,17 @@
 import os
 import json
 import getConfig
+from base.log import Log
+from base.baseExcel import BaseExcel
 
 
 class BaseCode:
 
     def __init__(self):
+        log = Log()
         self.LOCAL_READ_CONFIG = getConfig.GetConfig()
         self.PRO_DIR = getConfig.PROJECT_DIR
-
+        self.logger = log.get_logger()
         self.case_No = 0
 
     def get_value_from_return_json(json, value1, value2):
@@ -40,4 +43,29 @@ class BaseCode:
     def get_test_case_path(self):
         return os.path.join(self.PRO_DIR, self.LOCAL_READ_CONFIG.get_excel("excel_file"))
 
+    def get_case_data(self, sheet_name):
+        """
+        获取Excel 中的接口测试用例数据
+        :param sheet_name:
+        :return:
+        """
+        locals_get_config = getConfig.GetConfig()
 
+        case_dict = {}
+        excel_file = BaseExcel(self.get_test_case_path)
+        # 获取指定 sheet数据
+        sheet = excel_file.get_sheet(sheet_name)
+        # 获取行数
+        nrows = excel_file.get_rows(sheet_name)
+        # 获取列号
+        ncols = excel_file.get_cols(sheet_name)
+
+        for i in range(nrows):
+            test_name = excel_file.get_content(sheet_name, i, self.case_name)
+            test_front_sql = excel_file.get_content(sheet_name, i, self.case_front_sql)
+            test_case_uri = excel_file.get_content(sheet_name, i, self.case_uri)
+            test_case_data = excel_file.get_content(sheet_name, i, self.case_data)
+            test_case_method = excel_file.get_content(sheet_name, i, self.case_method)
+            test_case_code = excel_file.get_content(sheet_name, i, self.case_code)
+            case_data = {'caseName': test_name, 'caseFrontSQL': test_front_sql, "caseUri": test_case_uri,
+                         "caseData": test_case_data,""}
